fc24007cb58a11ded70f860e19d0ee552fba633f
Rostyslav Burlaka <ukrdentist@gmail.com>
2025-06-29 12:43:26 +0300

    feat(DART-08): complete task 1

--- lib/dart_collections_journey.dart
+++ lib/dart_collections_journey.dart
@@ -1,3 +1,34 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:math';
+void main() {
+  runTask1();
 }
+void runTask1() {
+  print('------------------- Task 1 -------------------');
+
+  final random = Random();
+  final numbers = List.generate(100, (index) => random.nextInt(101));
+
+  print(numbers);
+
+  print('65-й елемент: ${numbers[64]}');
+
+  numbers.insert(49, 1000000000);
+
+  numbers.removeWhere((n) => n == 24 || n == 45 || n == 66 || n == 88);
+
+  int sum = 0;
+  for (int i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 3 == 0) {
+      sum += numbers[i];
+    }
+  }
+  print('Сума чисел, що діляться на 3: $sum');
+
+  List<int> temp = [];
+  for (var n in numbers) {
+    if (n % 2 == 0) {
+      temp.add(n);
+    }
+  }
+  print('Кількість парних чисел у списку temp: ${temp.length}');
+} //
